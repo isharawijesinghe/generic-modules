9f2158f63bee31b8cebe823d1043abe7f18cf143
IsharaWijesinghe <ishara.wijesinghe@trustpayments.com>
2024-08-16 00:07:28 +0530

    Module update

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -2,6 +2,10 @@ output "lambda_function_name" {
   value = aws_lambda_function.lambda.function_name
 }
 
+output "lambda_function_arn" {
+  value = aws_lambda_function.lambda.arn
+}
+
 output "lambda_alias_name" {
   value = aws_lambda_alias.lambda_alias.name
 }
