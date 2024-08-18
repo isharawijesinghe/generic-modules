e2396a41263459d9af19c3439749c3f529b48339
IsharaWijesinghe <ishara.wijesinghe@trustpayments.com>
2024-08-18 21:48:47 +0530

    Api Gw Module Updated

--- terraform/modules/apigw/apigw_stages/main.tf
+++ terraform/modules/apigw/apigw_stages/main.tf
@@ -6,35 +6,35 @@ resource "aws_api_gateway_stage" "stage" {
   tags = var.tags
 }
 
-resource "aws_api_gateway_method_settings" "method_settings" {
-  rest_api_id = var.apigw_rest_api_id
-  stage_name  = aws_api_gateway_stage.stage.stage_name
-  method_path = "*/*"
-
-  settings {
-    metrics_enabled      = var.stage.method_settings.metrics_enabled
-    logging_level        = var.stage.method_settings.logging_level
-    caching_enabled      = var.stage.method_settings.caching_enabled
-    cache_data_encrypted = var.stage.method_settings.cache_data_encrypted
-  }
-
-  depends_on = [aws_api_gateway_stage.stage]
-}
-
-resource "aws_api_gateway_usage_plan" "usage_plan" {
-  count = var.stage.throttle_enabled ? 1 : 0
-  name = "${var.environment}-${var.name}-usage-plan"
-
-  api_stages {
-    api_id = var.apigw_rest_api_id
-    stage  = aws_api_gateway_stage.stage.stage_name
-  }
-
-  throttle_settings {
-    burst_limit = var.stage.throttle_settings.burst_limit
-    rate_limit  = var.stage.throttle_settings.rate_limit
-  }
-}
+# resource "aws_api_gateway_method_settings" "method_settings" {
+#   rest_api_id = var.apigw_rest_api_id
+#   stage_name  = aws_api_gateway_stage.stage.stage_name
+#   method_path = "*/*"
+#
+#   settings {
+#     metrics_enabled      = var.stage.method_settings.metrics_enabled
+#     logging_level        = var.stage.method_settings.logging_level
+#     caching_enabled      = var.stage.method_settings.caching_enabled
+#     cache_data_encrypted = var.stage.method_settings.cache_data_encrypted
+#   }
+#
+#   depends_on = [aws_api_gateway_stage.stage]
+# }
+#
+# resource "aws_api_gateway_usage_plan" "usage_plan" {
+#   count = var.stage.throttle_enabled ? 1 : 0
+#   name = "${var.environment}-${var.name}-usage-plan"
+#
+#   api_stages {
+#     api_id = var.apigw_rest_api_id
+#     stage  = aws_api_gateway_stage.stage.stage_name
+#   }
+#
+#   throttle_settings {
+#     burst_limit = var.stage.throttle_settings.burst_limit
+#     rate_limit  = var.stage.throttle_settings.rate_limit
+#   }
+# }
 
 resource "aws_api_gateway_vpc_link" "vpc_link" {
   count       = var.stage.vpc_link_name != null ? 1 : 0
